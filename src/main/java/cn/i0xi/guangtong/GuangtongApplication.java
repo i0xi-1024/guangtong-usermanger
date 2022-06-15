84462d1e43fd413ef117dee5f7b44620e51f337c
i零析 <lx@i0xi.cn>
2022-06-15 16:46:44 +0800

    Initial commit

---
+++ src/main/java/cn/i0xi/guangtong/GuangtongApplication.java
@@ -0,0 +1,13 @@
+package cn.i0xi.guangtong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuangtongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuangtongApplication.class, args);
+	}
+
+}
