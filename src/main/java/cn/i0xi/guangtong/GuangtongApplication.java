79fca7e4494a07fbc9d5baf6ea8621e28502b84e
i零析 <lx@i0xi.cn>
2022-06-15 16:47:28 +0800

    Changes

--- src/main/java/cn/i0xi/guangtong/GuangtongApplication.java
+++ src/main/java/cn/i0xi/guangtong/GuangtongApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuangtongApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GuangtongApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GuangtongApplication.class, args);
+    }
 
 }
--- src/test/java/cn/i0xi/guangtong/GuangtongApplicationTests.java
+++ src/test/java/cn/i0xi/guangtong/GuangtongApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GuangtongApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
