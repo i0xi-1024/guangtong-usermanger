94e1a1182ec562928e03fef5dbb0a9f434fefed9
i0xi <114292100@qq.com>
2022-12-01 19:15:56 +0800

    some updates

--- src/main/java/cn/i0xi/guangtong/controller/HelloController.java
+++ src/main/java/cn/i0xi/guangtong/controller/HelloController.java
@@ -1,23 +1,22 @@
 package cn.i0xi.guangtong.controller;
 
+import cn.i0xi.guangtong.domain.Users;
 import cn.i0xi.guangtong.service.UsersService;
 import com.google.gson.Gson;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
-@ResponseBody
 public class HelloController {
 
 
     @Resource
     UsersService usersService;
     @RequestMapping("/hello")
-    public String hello() {
-        return new Gson().toJson(usersService.list());
+    public List<Users> hello() {
+        return usersService.list();
     }
 }
