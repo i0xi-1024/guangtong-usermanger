7c12bd07b66179828f4397b50d2c107e5e98fa50
i0xi <114292100@qq.com>
2022-12-30 17:32:29 +0800

    Changes

--- src/main/java/cn/i0xi/guangtong/controller/HelloController.java
+++ src/main/java/cn/i0xi/guangtong/controller/HelloController.java
@@ -2,6 +2,8 @@ package cn.i0xi.guangtong.controller;
 
 import cn.i0xi.guangtong.domain.Users;
 import cn.i0xi.guangtong.service.UsersService;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +11,16 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class HelloController {
-
 
     @Resource
     UsersService usersService;
     @RequestMapping("/hello")
     public List<Users> hello() {
-        return usersService.list();
+        List<Users> users = usersService.list();
+        log.info(users.toString());
+        return users;
     }
 
     @RequestMapping("/addUsers")
